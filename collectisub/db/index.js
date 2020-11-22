37b2e4a40f41f0ddf28ce3b514653f3d4ee3e2da
fiffu <fhejehfif@gmail.com>
2020-11-22 10:19:47 +0800

    Enable Postgres connection

--- collectisub/db/index.js
+++ collectisub/db/index.js
@@ -3,8 +3,8 @@ const InMemoryDao = require('./adaptor-memory-map');
 
 function connect() {
     try {
-        // return new PostgresDao();
-        throw new Error('intentionally forcing in-memory use');
+        return new PostgresDao();
+        // throw new Error('intentionally forcing in-memory use');
     } catch (ex) {
         const msg = ex.message || ex;
         console.warn(`Postgres adaptor failed (${msg}), falling back to in-memory db`);
